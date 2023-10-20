5accc0e615e8aebbdf01f19a8397dbe64f63e203
Mattemcbee <116092337+Mattemcbee@users.noreply.github.com>
2023-10-19 20:38:16 -0400

    Add files via upload

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,19 +10,37 @@ import React from 'react';
 import Run from '../images/run.jpg'
 
 
-
 const Footer = () => {
 
   return (
-    <Container fluid className="tinyTitle" style={{ paddingBottom: '20px' }}>
+    <Container fluid className="tinyTitle" style={{ paddingBottom: '20px', height:'100%' }}>
       <Row>
-        <Col xs='' className=""
+        <Col xs='1' style={{verticalAlign:'center' , textAlign:'center', width:'100%', padding:'auto' }}>
+          <a className=' centerBox2' style={{ color: 'inherit' }} href='https://disposedkings.netlify.app/'>
+            <i className="fab fa-facebook centerBox2" />
+          </a>
+        </Col>
+        <Col xs='1'>
+          <a className=' centerBox2' style={{ color: 'inherit' }} href='https://disposedkings.netlify.app/'>
+            <i className="fab fa-apple centerBox2" />
+          </a>
+        </Col>
+        <Col xs='6' className=""
           style={{ margin: 'auto' }}
         >
-       <img className="titleImage"/>   
-
+          <img className="titleImage" />
         </Col>
-        
+        <Col xs='1'>
+          <a className=' centerBox2' style={{ color: 'inherit' }} href='https://disposedkings.netlify.app/'>
+            <i className="fab fa-instagram centerBox2" />
+          </a>
+        </Col>
+        <Col xs='1'>
+          <a className=' centerBox2' style={{ color: 'inherit' }} href='https://disposedkings.netlify.app/'>
+            <i className="fab fa-youtube centerBox2" />
+          </a>
+        </Col>
+
 
 
       </Row>
